c6ed7f3f5233c7e390d5573a563b1b328e29f08e
WHHoward <2856017412@qq.com>
2022-04-03 12:10:57 +0800

    time02

--- list.h
+++ list.h
@@ -66,10 +66,7 @@ status GetElem(SqList L,int i,ElemType &e)
 }
 
 int LocateElem(SqList L,ElemType e)
-// 如果线性表L存在，查找元素e在线性表L中的位置序号并返回该序号；如果e不存在，返回0；当线性表L不存在时，返回INFEASIBLE（即-1）。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(L.elem)
     {
         for(int i = 0; i < L.length; i++)
@@ -77,14 +74,10 @@ int LocateElem(SqList L,ElemType e)
         return 0;   
     }
     return INFEASIBLE;
-    /********** End **********/
 }
 
 status PriorElem(SqList L,ElemType e,ElemType &pre)
-// 如果线性表L存在，获取线性表L中元素e的前驱，保存在pre中，返回OK；如果没有前驱，返回ERROR；如果线性表L不存在，返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(L.elem)
     {
         for(int i = 0; i < L.length; i++)
@@ -101,15 +94,10 @@ status PriorElem(SqList L,ElemType e,ElemType &pre)
         return ERROR;
     }
     return INFEASIBLE;
-
-    /********** End **********/
 }
 
 status NextElem(SqList L,ElemType e,ElemType &next)
-// 如果线性表L存在，获取线性表L元素e的后继，保存在next中，返回OK；如果没有后继，返回ERROR；如果线性表L不存在，返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(L.elem)
     {
         for(int i = 0; i < L.length; i++)
@@ -127,14 +115,10 @@ status NextElem(SqList L,ElemType e,ElemType &next)
     }
     return INFEASIBLE;
 
-    /********** End **********/
 }
 
 status ListInsert(SqList &L,int i,ElemType e)
-// 如果线性表L存在，将元素e插入到线性表L的第i个元素之前，返回OK；当插入位置不正确时，返回ERROR；如果线性表L不存在，返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(!L.elem) return INFEASIBLE;
     else
     {
@@ -155,14 +139,10 @@ status ListInsert(SqList &L,int i,ElemType e)
             return OK;
         }
     }
-    /********** End **********/
 }
 
 status ListDelete(SqList &L,int i,ElemType &e)
-// 如果线性表L存在，删除线性表L的第i个元素，并保存在e中，返回OK；当删除位置不正确时，返回ERROR；如果线性表L不存在，返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(!L.elem)     return INFEASIBLE;
     else
     {
@@ -176,15 +156,10 @@ status ListDelete(SqList &L,int i,ElemType &e)
             return OK;
         }
     }  
-
-    /********** End **********/
 }
 
 status ListTraverse(SqList L)
-// 如果线性表L存在，依次显示线性表中的元素，每个元素间空一格，返回OK；如果线性表L不存在，返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(!L.elem) return INFEASIBLE;
     else
     {
@@ -194,15 +169,10 @@ status ListTraverse(SqList L)
         printf("%d",L.elem[L.length - 1]);
         return OK;
     }
-
-    /********** End **********/
 }
 
 status  SaveList(SqList L,char FileName[])
-// 如果线性表L存在，将线性表L的的元素写到FileName文件中，返回OK，否则返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(!L.elem) return INFEASIBLE;
     FILE *p;
     p = fopen(FileName,"w");
@@ -210,14 +180,10 @@ status  SaveList(SqList L,char FileName[])
         fprintf(p,"%d",L.elem[i]);
     fclose(p);
     return OK;
-    /********** End **********/
 }
 
 status  LoadList(SqList &L,char FileName[])
-// 如果线性表L不存在，将FileName文件中的数据读入到线性表L中，返回OK，否则返回INFEASIBLE。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     if(L.elem)  return INFEASIBLE;
     else
     {
@@ -235,30 +201,19 @@ status  LoadList(SqList &L,char FileName[])
         fclose(p);
         return OK;
     }
-
-    /********** End **********/
 }
 
 status AddList(LISTS &Lists,char ListName[])
-// 只需要在Lists中增加一个名称为ListName的空线性表，线性表数据又后台测试程序插入。
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     SqList temp;
     temp.elem = (int *)malloc(sizeof(int) * LIST_INIT_SIZE);
     temp.length = 0;
     temp.listsize = LIST_INIT_SIZE;
     Lists.elem[Lists.length++].L = temp;
     strcpy(Lists.elem[Lists.length-1].name,ListName);
-    /********** End **********/
 }
-
-#include<string.h>
 status RemoveList(LISTS &Lists,char ListName[])
-// Lists中删除一个名称为ListName的线性表
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     for(int i = 0; i < Lists.length; i++)
     {
         if(strcmp(Lists.elem[i].name,ListName) == 0)
@@ -270,21 +225,15 @@ status RemoveList(LISTS &Lists,char ListName[])
         }
     }
     return ERROR;
-    /********** End **********/
 }
 
 #include<string.h>
 int LocateList(LISTS Lists,char ListName[])
-// 在Lists中查找一个名称为ListName的线性表，成功返回逻辑序号，否则返回0
 {
-    // 请在这里补充代码，完成本关任务
-    /********** Begin *********/
     for(int i = 0; i < Lists.length; i++)
     {
         if(strcmp(Lists.elem[i].name,ListName) == 0)
             return i + 1;
     }
     return 0;
-
-    /********** End **********/
 }
